8ffbbfd09f1b24802180eff879898f186d16f42f
John Ferguson Smart <john.smart@wakaleo.com>
2022-08-12 20:39:31 +1000

    Activated Algolia search engine

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -140,6 +140,29 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'V10PILA8X2',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '722dc898d5026266faadec59963be60a',
+  
+        indexName: 'serenity-bdd',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
     }),
 
   plugins: [
