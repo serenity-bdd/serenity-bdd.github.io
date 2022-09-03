6f6bf7ded123f7f2f589338dda2b0ed2011228f3
vivganes <vivek@vivekganesan.com>
2022-09-03 19:58:49 +0530

    fix overflowing button on small screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="docs/tutorials/first_test">
+          <Link className="button button--secondary" to="docs/tutorials/first_test">
             Write Your First Serenity BDD Test in under 10 min ⏱️
           </Link>
         </div>
