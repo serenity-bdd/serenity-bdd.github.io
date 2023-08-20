50bea02fd281164e317d80ee23c791bcc8063d16
Andreas Worm <andreas.worm@headissue.com>
2023-08-20 12:10:34 +0200

    simplify link to older doc source code

--- src/components/LegacyLink404.js
+++ src/components/LegacyLink404.js
@@ -5,9 +5,7 @@ import {useLocation} from '@docusaurus/router';
 
 export default function LegacyLink404() {
     const location = useLocation()
-    const legacySource =
-        "https://github.com/serenity-bdd/the-serenity-book/tree/master/modules/ROOT/pages/" +
-        location.pathname.split("/").reduce((a, b) => b).replace("html", "adoc")
+    const legacySource = "https://github.com/serenity-bdd/the-serenity-book"
     return (
         <Layout
             title={`404`}
@@ -27,11 +25,10 @@ export default function LegacyLink404() {
                         Please use the header links to get to the latest content.
                     </p>
                     <p>
-                        If there is anything missing in the new documentation, please let us know or add it yourself.
-                    </p>
+                        Try searching in the <a href={legacySource}> old documenation</a>
+                    </p> 
                     <p>
-                        Legacy source might be found at <a
-                        href={legacySource}>{legacySource}</a>
+                        If there is anything missing in the new documentation, please let us know or add it yourself.
                     </p>
                 </div>
             </div>
